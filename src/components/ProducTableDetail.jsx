ee042d79b87b17b98a4634c8acd63052ed5d5ae0
Hancer <hancermercedes@gmail.com>
2023-12-20 10:33:37 -0400

    Adding the edit funcionality

--- src/components/ProducTableDetail.jsx
+++ src/components/ProducTableDetail.jsx
@@ -4,18 +4,21 @@ import { CiTrash } from "react-icons/ci";
 import { DollarFormat } from "../utils/DollarFormat";
 import PropTypes from "prop-types";
 
-export const ProducTableDetail = ({ product, removeProduct }) => {
+export const ProducTableDetail = ({ product, removeProduct, editProduct }) => {
   return (
     <tr className={tablestyle.tr}>
       <td className={tablestyle.td}>{product.productId}</td>
       <td className={tablestyle.td}>{product.description}</td>
       <td className={tablestyle.td}>{DollarFormat.format(product.price)}</td>
-      <td className={tablestyle.td}>{product.cuantity}</td>
+      <td className={tablestyle.td}>{product.quantity}</td>
       <td className={tablestyle.td}>{DollarFormat.format(product.subtotal)}</td>
       <td className={tablestyle.td}>{DollarFormat.format(product.total)}</td>
       <td className={tablestyle.td}>
         <div className={tablestyle.button_wrapper}>
-          <button className={tablestyle.button_primary}>
+          <button
+            className={tablestyle.button_primary}
+            onClick={() => editProduct(product)}
+          >
             <LuClipboardEdit size={15} />
           </button>
 
@@ -34,4 +37,5 @@ export const ProducTableDetail = ({ product, removeProduct }) => {
 ProducTableDetail.propTypes = {
   product: PropTypes.object.isRequired,
   removeProduct: PropTypes.func.isRequired,
+  editProduct: PropTypes.func.isRequired,
 };
--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -5,7 +5,6 @@ import { MdOutlineCleaningServices } from "react-icons/md";
 import tablestyle from "../CustomStyles/Table.module.css";
 import { useEffect, useState } from "react";
 import { ProducTableDetail } from "./ProducTableDetail";
-import { AutoCalculateColumn } from "../utils/AutoCalculate";
 import { DollarFormat } from "../utils/DollarFormat";
 
 export const Sales = () => {
@@ -17,11 +16,22 @@ export const Sales = () => {
   const [DNI, setDNI] = useState("");
 
   // products states
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]); // An Array of objects
+  const [edittingProducts, setEdittingProducts] = useState(null); // A specifiq object
+  const [productId, setProductId] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState();
+  const [quantity, setQuantity] = useState(0);
+  const [subtotal, setSubtotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  //Edit products states
+  const [editQuantity, setEditquantity] = useState(0);
+  const [editSubtotal, setEditSubtotal] = useState(0);
+  const [editTotal, setEditTotal] = useState(0);
 
   const addProduct = (product) => {
     if (product.productId === "" || product.productId === undefined) return;
-
     setProducts([...products, product]);
     onClear();
   };
@@ -31,16 +41,52 @@ export const Sales = () => {
     setProducts(products.filter((product) => product.productId !== productId));
   };
 
+  // Here i took the product to edit it.
+  const EditProduct = (product) => {
+    setEdittingProducts(product);
+    removeProduct(product.productId);
+  };
+
+  // Here i added the edited product to the list again.
+  const AddEditedProduct = (product) => {
+    product.quantity = editQuantity;
+    product.subtotal = editSubtotal;
+    product.total = editTotal;
+    addProduct(product);
+    setEdittingProducts(null);
+  };
+
   useEffect(() => {
     localStorage.setItem("products", JSON.stringify(products));
   }, [products]);
 
-  const [productId, setProductId] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState();
-  const [cuantity, setCuantity] = useState();
-  const [subtotal, setSubtotal] = useState();
-  const [total, setTotal] = useState();
+  const handleChange = (ev) => {
+    const _quantity = ev.target.value;
+    setQuantity(_quantity);
+    const newValue = _quantity * price;
+    setSubtotal(newValue);
+  };
+
+  // Here i changed the Editquantity with the new value
+  const handleEditChange = (ev) => {
+    const _quantity = ev.target.value;
+    setEditquantity(_quantity);
+    const newValue = _quantity * edittingProducts.price;
+    setEditSubtotal(newValue);
+  };
+
+  // Here i triggered an effect to calculate the total when the sub-total change.
+  useEffect(() => {
+    const tax = subtotal * 0.18;
+    const newValue = subtotal + tax;
+    setTotal(newValue);
+  }, [subtotal]);
+  // Here i triggered an effect to calculate the new total when the sub-total is editted.
+  useEffect(() => {
+    const tax = editSubtotal * 0.18;
+    const newValue = editSubtotal + tax;
+    setEditTotal(newValue);
+  }, [editSubtotal]);
 
   const OnDelete = (env) => {
     env.preventDefault();
@@ -55,16 +101,20 @@ export const Sales = () => {
     setProductId("");
     setDescription("");
     setPrice();
-    setCuantity();
-    setSubtotal();
-    setTotal();
+    setQuantity(0);
+    setSubtotal(0);
+    setTotal(0);
+
+    setEditquantity(0);
+    setEditSubtotal(0);
+    setEditTotal(0);
+    setEdittingProducts(null);
   };
+
   // Here i made a sum for all the total, to have a global value.
   let globalTotal = 0;
   products.forEach((product) => {
-    let total = 0;
     globalTotal += product.total;
-    console.log(total);
   });
 
   return (
@@ -108,67 +158,147 @@ export const Sales = () => {
         </button>
       </div>
       <div className={style.product_container}>
-        <form className={style.product_form}>
-          <input
-            type="text"
-            placeholder="#Product"
-            value={productId}
-            onChange={(env) => setProductId(env.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Description"
-            value={description}
-            onChange={(env) => setDescription(env.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Price"
-            value={price ?? 0.0}
-            onChange={(env) => setPrice(env.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Cuantity"
-            value={cuantity ?? 0}
-            onChange={(env) => setCuantity(env.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Sub-Total"
-            value={subtotal ?? 0.0}
-            onChange={() => setSubtotal(price * cuantity)}
-          />
-          <input
-            type="number"
-            placeholder="Total"
-            value={total ?? 0.0}
-            onChange={() => setTotal(subtotal * 0.18 + subtotal)}
-          />
-        </form>
-        <div className={tablestyle.button_wrapper}>
-          <button className={style.btn_clean} onClick={onClear}>
-            <MdOutlineCleaningServices size={20} />
-            Clear
-          </button>
-          <button
-            className={style.btn_add}
-            type="submit"
-            onClick={() =>
-              addProduct({
-                productId,
-                description,
-                price,
-                cuantity,
-                subtotal,
-                total,
-              })
-            }
-          >
-            <MdAddShoppingCart size={20} />
-            Add to cart
-          </button>
-        </div>
+        {edittingProducts ? (
+          <>
+            <form className={style.product_form}>
+              <input
+                type="text"
+                placeholder="#Product"
+                value={edittingProducts.productId}
+                onChange={(ev) =>
+                  setEdittingProducts({
+                    ...edittingProducts,
+                    productId: ev.target.value,
+                  })
+                }
+              />
+              <input
+                type="text"
+                placeholder="Description"
+                value={edittingProducts.description}
+                onChange={(ev) =>
+                  setEdittingProducts({
+                    ...edittingProducts,
+                    description: ev.target.value,
+                  })
+                }
+              />
+              <input
+                type="number"
+                placeholder="Price"
+                value={edittingProducts.price}
+                onChange={(ev) =>
+                  setEdittingProducts({
+                    ...edittingProducts,
+                    price: ev.target.value,
+                  })
+                }
+              />
+              <input
+                type="number"
+                placeholder="Quantity"
+                value={editQuantity}
+                onChange={handleEditChange}
+              />
+              <input
+                type="number"
+                placeholder="Sub-Total"
+                value={editSubtotal}
+                readOnly={true}
+              />
+              <input
+                type="number"
+                placeholder="Total"
+                value={editTotal}
+                readOnly={true}
+              />
+            </form>
+            <div className={tablestyle.button_wrapper}>
+              <button className={style.btn_clean} onClick={onClear}>
+                <MdOutlineCleaningServices size={20} />
+                Clear
+              </button>
+              <button
+                className={style.btn_add}
+                type="submit"
+                onClick={() =>
+                  AddEditedProduct(edittingProducts, {
+                    editQuantity,
+                    editSubtotal,
+                    editTotal,
+                  })
+                }
+              >
+                <MdAddShoppingCart size={20} />
+                Add to cart
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <form className={style.product_form}>
+              <input
+                type="text"
+                placeholder="#Product"
+                value={productId}
+                onChange={(ev) => setProductId(ev.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={(env) => setDescription(env.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="Price"
+                value={price ?? 0.0}
+                onChange={(env) => setPrice(env.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="Quantity"
+                value={quantity}
+                onChange={handleChange}
+              />
+              <input
+                type="number"
+                placeholder="Sub-Total"
+                value={subtotal}
+                readOnly={true}
+              />
+              <input
+                type="number"
+                placeholder="Total"
+                value={total}
+                readOnly={true}
+              />
+            </form>
+            <div className={tablestyle.button_wrapper}>
+              <button className={style.btn_clean} onClick={onClear}>
+                <MdOutlineCleaningServices size={20} />
+                Clear
+              </button>
+              <button
+                className={style.btn_add}
+                type="submit"
+                onClick={() =>
+                  addProduct({
+                    productId,
+                    description,
+                    price,
+                    quantity,
+                    subtotal,
+                    total,
+                  })
+                }
+              >
+                <MdAddShoppingCart size={20} />
+                Add to cart
+              </button>
+            </div>
+          </>
+        )}
       </div>
 
       <div className={style.product_grid_container}>
@@ -179,7 +309,7 @@ export const Sales = () => {
                 <th className={tablestyle.th}>#Product</th>
                 <th className={tablestyle.th}>Description</th>
                 <th className={tablestyle.th}>Price</th>
-                <th className={tablestyle.th}>Cuantity</th>
+                <th className={tablestyle.th}>Quantity</th>
                 <th className={tablestyle.th}>Sub-Total</th>
                 <th className={tablestyle.th}>Total</th>
                 <th className={tablestyle.th}>Actions</th>
@@ -191,12 +321,13 @@ export const Sales = () => {
                   key={index}
                   product={product}
                   removeProduct={removeProduct}
+                  editProduct={EditProduct}
                 />
               ))}
             </tbody>
             <tfoot className={tablestyle.thead_grey}>
               <tr>
-                <td colSpan={7}>Total:{DollarFormat.format(globalTotal)}</td>
+                <td colSpan={7}>TOTAL:{DollarFormat.format(globalTotal)}</td>
               </tr>
             </tfoot>
           </table>
